c39b20de28d83867a8aee810932e1249d701715b
Noah Schoonover <noah@skoonie.com>
2016-01-26 20:26:48 -0700

    Changed testLabel in MainView class to "Soup"
    Signed-off-by: Noah Schoonover <noah@skoonie.com>

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -121,7 +121,7 @@ public class MainView extends JFrame implements CommandHandler
         //be removed, allowing the mainPanel to wrap to its contents
         Tools.setSizes(mainPanel, 600, 600);
         
-        JLabel testLabel = new JLabel("Test.");
+        JLabel testLabel = new JLabel("Soup");
         testLabel.setAlignmentX(LEFT_ALIGNMENT);
         mainPanel.add(testLabel);
         //DEBUG NPS// -- should be removed later
